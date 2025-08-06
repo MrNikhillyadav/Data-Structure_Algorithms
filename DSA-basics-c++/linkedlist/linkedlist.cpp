da4fe0d6aff4cb1849ed8b0df9daedef24b769ea
MrNikhillYadav <facts.foundr@gmail.com>
2025-08-06 09:43:06 +0530

    add vector linkedlist

--- DSA-basics-c++/linkedlist/linkedlist.cpp
+++ DSA-basics-c++/linkedlist/linkedlist.cpp
@@ -1,89 +1,187 @@
+// #include <iostream>
+// using namespace std;
+
+// struct Node{
+//     int data;
+//     Node* next;
+//     Node(int val) : data(val), next(nullptr) {}
+// };
+
+// void printLinkedList(Node* head){
+//     Node* temp = head;
+
+//     while(temp != nullptr){
+//         cout << temp->data << endl;
+//         temp = temp -> next; 
+//     }
+// }
+
+
+// void insertAtHead(Node*& head, int val){
+
+//     Node* n = new Node(val);
+
+//     n -> next = head;
+//     head = n;
+
+// }
+
+// Node* getTail(Node* head){
+//     while(head->next != NULL){
+//         head = head -> next;
+//     }
+
+//     return head;
+// }
+
+// void insertAtTail(Node* head, int val){
+//     Node* n = new Node(val);
+//     Node* tail = getTail(head);
+//     tail -> next = n;
+// }
+
+// Node* getPrevNode(Node* head, int j){
+//     int k = 0;
+//     while(k < j){
+//         head = head -> next;
+//         k++;
+//     }
+
+//     return head;
+// }
+
+// void insertAtIndex(Node*& head, int index, int val){
+
+//     if(index == 0){
+//         insertAtHead(head,val);
+//         return;
+//     }
+
+//     Node* n = new Node(val);
+//     Node* prev = getPrevNode(head,index-1);
+
+//     if(prev == NULL){
+//         return;
+//     }
+
+//     n -> next = prev -> next;
+//     prev-> next = n;
+
+// }
+
+
+// int main(){
+
+    
+//     Node* head = new Node(1);
+//     head -> next = new Node(2);
+//     head -> next->next = new Node(3);
+    
+//     insertAtHead(head, 0);
+//     // printLinkedList(head);
+    
+//     insertAtTail(head,4);
+//     insertAtIndex(head,2,25);
+//     insertAtIndex(head,0,50);
+//     printLinkedList(head);
+
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
-struct Node{
-    int data;
-    Node* next;
-    Node(int val) : data(val), next(nullptr) {}
+class ListNode {
+    public :
+
+        int data;
+        ListNode* next;
+
+        ListNode ( int data ){
+            this->data = data;
+            this->next = NULL;
+        }
 };
 
-void printLinkedList(Node* head){
-    Node* temp = head;
+void printListNode(ListNode* head){
 
-    while(temp != nullptr){
-        cout << temp->data << endl;
-        temp = temp -> next; 
+    ListNode* temp = head;
+
+    while(temp != NULL){
+        cout << temp -> data ;
+        temp = temp -> next;
     }
 }
 
-
-void insertAtHead(Node*& head, int val){
-
-    Node* n = new Node(val);
-
+ListNode* insertAtHead( ListNode* head, int data){
+    ListNode* n = new ListNode(data);
     n -> next = head;
     head = n;
 
+    return head;
 }
 
-Node* getTail(Node* head){
-    while(head->next != NULL){
+ListNode* getTailNode(ListNode* head){
+    if(head == NULL){
+        return nullptr;
+    }
+
+    while( head -> next != NULL){
         head = head -> next;
     }
 
     return head;
 }
 
-void insertAtTail(Node* head, int val){
-    Node* n = new Node(val);
-    Node* tail = getTail(head);
+void insertAtTail(ListNode* head, int data){
+    ListNode* n = new ListNode(data);
+    ListNode* tail = getTailNode(head);
     tail -> next = n;
 }
 
-Node* getPrevNode(Node* head, int j){
+ListNode* getPrevNode(ListNode* head, int i){
     int k = 0;
-    while(k < j){
+    while(k < i ){
         head = head -> next;
         k++;
     }
 
     return head;
+
 }
 
-void insertAtIndex(Node*& head, int index, int val){
-
-    if(index == 0){
-        insertAtHead(head,val);
-        return;
-    }
-
-    Node* n = new Node(val);
-    Node* prev = getPrevNode(head,index-1);
+void insertAtIndex(ListNode* head, int index, int data){
+    ListNode* n = new ListNode(data);
+    ListNode* prev = getPrevNode(head,index -1);
 
     if(prev == NULL){
         return;
     }
-
     n -> next = prev -> next;
-    prev-> next = n;
-
+    prev ->next = n;
 }
-
 
 int main(){
 
-    
-    Node* head = new Node(1);
-    head -> next = new Node(2);
-    head -> next->next = new Node(3);
-    
-    insertAtHead(head, 0);
-    // printLinkedList(head);
-    
+    //create a node class
+    // create a list of node 1->2->3
+    // insert 0 at head
+    // insert 4 at the tail
+    // insert 25 at index 1
+
+    ListNode* head = new ListNode(1);
+    head -> next =  new ListNode(2);
+    head -> next-> next =  new ListNode(3);
+
+    head = insertAtHead(head, 0);
     insertAtTail(head,4);
-    insertAtIndex(head,2,25);
-    insertAtIndex(head,0,50);
-    printLinkedList(head);
+    insertAtIndex(head, 1, 25);
+
+    printListNode(head);
+
 
 
     return 0;
--- DSA-basics-c++/vector/vector.cpp
+++ DSA-basics-c++/vector/vector.cpp
@@ -2,41 +2,57 @@
 #include <vector>
 using namespace std;
 
+void printVector( vector<int> &v){
+    for(auto x : v){
+        cout << x  << endl;
+    }
+}
+
 int main(){
     // creating a vector
-    vector<int> numbers;
+    // vector<int> v = {10,20,30,40,50};
+    // printVector(v);
 
-    numbers.push_back(5);
-    numbers.push_back(8);
-    numbers.push_back(20);
-    numbers.push_back(12);
+    vector<int> v2(4,10);
+    printVector(v2);
+
+
+    // vector<int> numbers;
+
+    // numbers.push_back(5);
+    // numbers.push_back(8);
+    // numbers.push_back(20);
+    // numbers.push_back(12);
 
     //printing vectors
-    for(int i=0; i < numbers.size(); i++){
-        cout << numbers[i] << endl;
-    }
+    // for(int i=0; i < numbers.size(); i++){
+    //     cout << numbers[i] << endl;
+    // }
 
     //other way is :
-    for(auto x : numbers){
-        cout << x << endl;
-    }
+    // for(auto x : numbers){
+    //     cout << x << endl;
+    // }
 
-    numbers.pop_back();
+    // numbers.pop_back();
 
-    numbers.clear();
+    // numbers.clear();`
 
     
-    vector<string> ch;
+    // vector<string> ch;
 
-    ch.push_back("h");
-    ch.push_back("e");
-    ch.push_back("l");
-    ch.push_back("l");
-    ch.push_back("o");
+    // ch.push_back("h");
+    // ch.push_back("e");
+    // ch.push_back("l");
+    // ch.push_back("l");
+    // ch.push_back("o");
 
-    for(auto c : ch){
-        cout << c << " ";
-    }
+    // for(auto c : ch){
+    //     cout << c << " ";
+    // }
+
+
+
 
     return 0;
 }
