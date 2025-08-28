dbe1afb40cefe5b315f9724b8186ef6b8bfc71f2
MrNikhillYadav <facts.foundr@gmail.com>
2025-08-29 02:01:37 +0530

    add min-max, search in BST

---
+++ DSA-basics-c++/BST/search_in_bst.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+    public:
+        int data;
+        TreeNode* left;
+        TreeNode* right;
+
+        TreeNode(int data) {
+            this -> data = data;
+            this -> left = NULL;
+            this -> right = NULL; 
+        }
+};
+
+//search
+bool search(TreeNode* root, int key){
+    if(root == NULL) return false;
+
+    if(root->data == key){
+        return true;
+    }else if(key < root->data) {
+        return search(root->left, key);
+    }
+
+    return search(root->right, key);
+}
+
+int main(){
+
+   TreeNode* root = new TreeNode(10);
+
+   root->left = new TreeNode(5);
+   root->left->left = new TreeNode(3);
+   root->left->right = new TreeNode(7);
+
+   root->right = new TreeNode(15);
+   root->right->left = new TreeNode(13);
+   root->right->right = new TreeNode(17);
+
+   int key = 13;
+   search(root,key) ? cout << "true" << endl : cout << "false" << endl;
+
+    return 0;
+}
